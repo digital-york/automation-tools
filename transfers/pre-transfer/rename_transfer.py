c7e3250a81fd4ca89b6b766abcfd839d415c5a6b
Julie Allinson <julie.allinson@york.ac.uk>
2016-04-27 13:50:40 +0100

    tinkering

--- transfers/pre-transfer/rename_transfer.py
+++ transfers/pre-transfer/rename_transfer.py
@@ -12,14 +12,23 @@ def main(transfer_path):
     source = os.path.join(transfer_path)
     destination = os.path.join(transfer_path, 'objects')
     print('move data from' + source + ' to ' + destination)
+    shutil.copy(source, destination)
     shutil.copytree(source, destination, symlinks=False, ignore=ignore_patterns('objects','doco'))
 
     print('move submission documentation into the /metadata/submissionDocumentation')
     os.mkdir(transfer_path + '/metadata')
     os.mkdir(transfer_path + '/metadata/submissionDocumentation')
-    source = os.path.join(transfer_path + 'doco')
+    source = os.path.join(transfer_path + 'doco/')
     destination = os.path.join(transfer_path, 'metadata/submissionDocumentation')
     shutil.move(source, destination)
+    os.rmdir(source)
+
+    print('reset all file permissions')
+    for root, dirs, files in os.walk(transfer_path):
+        for d in dirs:
+            os.chown(os.path.join(root, d), 'archivematica', 'archivematica')
+        for f in files:
+            os.chown(os.path.join(root, f), 'archivematica', 'archivematica')
 
 if __name__ == '__main__':
     transfer_path = sys.argv[1]
