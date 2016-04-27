82a44cfe58ff5d3eaff918d2fb8e5c6c5e8f6f9d
Julie Allinson <julie.allinson@york.ac.uk>
2016-04-27 13:12:43 +0100

    tinkering

--- transfers/pre-transfer/rename_transfer.py
+++ transfers/pre-transfer/rename_transfer.py
@@ -4,22 +4,23 @@ from __future__ import print_function
 import os
 import shutil
 import sys
+from shutil import ignore_patterns
 
 def main(transfer_path):
     print('hello')
     print(transfer_path)
     source = os.path.join(transfer_path)
+    os.mkdir(transfer_path + '/objects')
     destination = os.path.join(transfer_path, 'objects')
     print('move data from' + source + ' to ' + destination)
-    shutil.copytree(source, destination, symlinks=False, ignore=None)
-    shutil.rmtree(transfer_path + 'objects/objects')
+    shutil.copytree(source, destination, symlinks=False, ignore=ignore_patterns('objects','doco'))
 
     print('move submission documentation into the /metadata/submissionDocumentation')
     os.mkdir(transfer_path + '/metadata')
     os.mkdir(transfer_path + '/metadata/submissionDocumentation')
-    source = os.path.join(transfer_path + '/objects/doco')
-    destination = os.path.join(transfer_path, '/metadata/submissionDocumentation')
-    shutil.copytree(source, destination, symlinks=False, ignore=None)
+    source = os.path.join(transfer_path + 'doco')
+    destination = os.path.join(transfer_path, 'metadata/submissionDocumentation')
+    shutil.move(source, destination, symlinks=False, ignore=None)
 
     print ('remove submission documentation from objects directory')
     shutil.rmtree((transfer_path + '/objects/doco'))
