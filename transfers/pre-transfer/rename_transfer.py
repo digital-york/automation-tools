059c135da39346767aba625a647022798dcfaa1a
Julie Allinson <julie.allinson@york.ac.uk>
2016-04-28 11:52:17 +0100

    tinkering

--- transfers/pre-transfer/rename_transfer.py
+++ transfers/pre-transfer/rename_transfer.py
@@ -25,7 +25,7 @@ def main(transfer_path):
             if name != 'objects':
                 if name != 'submissionDocumentation':
                     if name != 'processingMCP.xml':
-                        shutil.move(full_name, os.path.join(os.path.join(transfer_path, 'objects/' + name))
+                        shutil.move(full_name, os.path.join(transfer_path, 'objects/'))
         except OSError as e:
             print(e)
 
