318ceaf80be3a98ba96925669ac8e7e543006a23
Julie Allinson <julie.allinson@york.ac.uk>
2016-05-24 17:58:26 +0100

    adding status

--- transfers/status/update_status.py
+++ transfers/status/update_status.py
@@ -15,7 +15,7 @@ def main(transfer_path,sip_uuid):
     # call archivematica api to get aip_uuid
     # call archivematica api to get status
 
-    print('http://192.168.168.192/api/transfer/status/%s/?username=geekscruff&api_key=66f63aa9f4d9c005e8920c588267f126c0d53bff',sip_uud)
+    print("http://192.168.168.192/api/transfer/status/%s/?username=geekscruff&api_key=66f63aa9f4d9c005e8920c588267f126c0d53bff",sip_uud)
     #
 
 if __name__ == '__main__':
