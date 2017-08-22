2ace4f2c854a854c831c6e90e5084ee5d01680a9
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2017-08-22 15:14:52 +0000

    raise exception when call json url fails

--- transfers/status.py
+++ transfers/status.py
@@ -129,6 +129,7 @@ def _call_url_json(url, params, method):
         response = requests.post(url, data=json.dumps(params), verify=False)
     LOGGER.debug('Response: %s', response)
     if not response.ok:
+        raise Exception('Request to %s returned %s %s', url, response.status_code, response.reason)
         LOGGER.error('Request to %s returned %s %s', url, response.status_code, response.reason)
         log_error('Request to %s returned %s %s', url, response.status_code, response.reason)
         LOGGER.debug('Response: %s', response.text)
